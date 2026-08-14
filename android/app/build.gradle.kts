c3bdc7f6c2937df65719c4055e5ee5af2d81ad53
skragzombi666 <skragzombi666@gmail.com>
2026-08-14 18:35:18 +0200

    Enable Java API desugaring for Android alpha

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,7 +25,12 @@ android {
     }
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
