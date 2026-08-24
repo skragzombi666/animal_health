be633bec94c9e64d9516bacd9b4fd4d9ac0866a7
skragzombi666 <skragzombi666@gmail.com>
2026-08-24 13:16:55 +0200

    Wire 0.9.17 ordering API into integration

--- custom_components/animal_health/__init__.py
+++ custom_components/animal_health/__init__.py
@@ -61,6 +61,7 @@ from .v0913_features import async_initialize_v0913_features, async_setup_v0913_f
 from .v0913_patches import apply_v0913_patches
 from .v0915_features import async_initialize_v0915_features, async_setup_v0915_features
 from .v0916_migration import async_migrate_v0916_task_kinds
+from .v0917_features import async_initialize_v0917_features, async_setup_v0917_features
 
 PLATFORMS = [
     Platform.SENSOR,
@@ -104,6 +105,7 @@ async def async_setup(hass: HomeAssistant, config: dict[str, Any]) -> bool:
     async_setup_v0912_task_links(hass)
     async_setup_v0913_features(hass)
     async_setup_v0915_features(hass)
+    async_setup_v0917_features(hass)
     async_setup_v082_features(hass)
     async_setup_v083_features(hass)
     async_setup_v084_features(hass)
@@ -146,6 +148,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: AnimalHealthConfigEntry)
     await async_initialize_v0912_features(hass)
     await async_initialize_v0913_features(hass)
     await async_initialize_v0915_features(hass)
+    await async_initialize_v0917_features(hass)
     await async_load_confirmation_policy_settings(hass)
 
     coordinator = AnimalHealthCoordinator(hass, database)
